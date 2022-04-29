c1246392d79e8c07ec14bc8b259bc89cebc0d91a
Thuyen Vu <thuyenvu@Thuyens-MBP.wireless.yale.internal>
2022-04-28 20:48:52 -0400

    started on timeline

--- src/pages/timeline/index.js
+++ src/pages/timeline/index.js
@@ -1,10 +1,197 @@
 import React from 'react'
+import { Chrono } from "react-chrono"
 
 export default function Timeline({props}){
-	return (
-		<div>
-			<h1>Timeline</h1>
-			<h1>Hello world</h1>
-		</div>
-	)
-}
+	const items1 = [{
+		title: "May 1940",
+		cardTitle: "1",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "2",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "3",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "4",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "5",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "7",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  }
+	];
+
+	const items2 = [{
+		title: "May 1940",
+		cardTitle: "8",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "9",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "10",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "11",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "12",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  },
+	  {
+		title: "May 1940",
+		cardTitle: "13",
+		url: "http://www.history.com",
+		cardSubtitle:"dura europos",
+		cardDetailedText: "issues of interpretation",
+		media: {
+		  type: "IMAGE",
+		  source: {
+			url: "https://upload.wikimedia.org/wikipedia/commons/3/32/DuraEuropos-TempleOfBel.jpg"
+		  }
+		}
+	  }
+	];
+  
+	  return (
+		  <> <div style = {{justifyContent:'center', alignItems:'center'}}>
+
+		<div style={{ width: "1000px", height: "500px", justifyContent:'center', alignItems:'center'}}>
+			<Chrono
+  items={items1}
+  theme={{
+    primary: "black",
+    secondary: "#d5f4e6",
+    cardBgColor: "#80ced6",
+    cardForeColor: "#80ced6",
+    titleColor: "#618685"
+  }}
+  mode="HORIZONTAL"
+/>
+
+		  </div><div style={{ width: "1000px", height: "500px", justifyContent:'center', alignItems:'center' }}>
+		  <Chrono
+  items={items2}
+  theme={{
+    primary: "black",
+    secondary: "#d5f4e6",
+    cardBgColor: "#80ced6",
+    cardForeColor: "#80ced6",
+    titleColor: "#618685"
+  }}
+  mode="HORIZONTAL"
+  itemPosition = "CENTER"
+/>
+			  </div>
+			  </div> </>
+	  )
+	}
